fe7e009b286e15f66f484dfbdbdde508df57ed1c
Veniamin <Weniaminwelichko@ukr.net>
2020-08-03 02:37:56 +0300

    Complete Condition-Based tasks part

--- js_hw/index.js
+++ js_hw/index.js
@@ -15,14 +15,14 @@ console.log(e + f);
 
 //Fourth task
 let nam = 11;
-let bool_t = true;
+let boolTrue = true;
 let language = 'java script';
-let van_hundred = '100';
+let vanHundred = '100';
 
 console.log(`nam: ${nam}`);
-console.log(`bool_t: ${bool_t}`);
+console.log(`bool_t: ${boolTrue}`);
 console.log(`language: ${language}`);
-console.log(`van_hundred: ${van_hundred}`);
+console.log(`van_hundred: ${vanHundred}`);
 
 //Fifth task
 let num = 1;
@@ -37,23 +37,25 @@ num--;
 
 //Prompt tasks
 //First task
-let usr_nam;
+let usrNam, squareUsrNam;
 
-usr_nam = prompt('Enter namber');
+usrNam = prompt('Enter namber');
 
-usr_nam = usr_nam ** 2;
+squareUsrNam = usrNam ** 2;
+
+alert(`Square of number ${usrNam}:${squareUsrNam}`)
 
 //Second task
-let first_usr_nam, second_usr_nam;
+let firstUsrNam, secondUsrNam;
 let average;
 
-first_usr_nam = prompt('Enter first namber');
-second_usr_nam = prompt('Enter second namber');
+firstUsrNam = prompt('Enter first namber');
+secondUsrNam = prompt('Enter second namber');
 
-first_usr_nam = parseFloat(first_usr_nam);
-second_usr_nam = parseFloat(second_usr_nam);
+firstUsrNam = parseFloat(firstUsrNam);
+secondUsrNam = parseFloat(secondUsrNam);
 
-average = (first_usr_nam + second_usr_nam) / 2;
+average = (firstUsrNam + secondUsrNam) / 2;
 
 alert(`Average is:${average}`);
 
@@ -64,6 +66,8 @@ minutes = prompt('Enter the number of minutes');
 
 seconds = minutes * 60;
 
+alert(`${seconds} seconds in ${minutes}`)
+
 //Fourth task
 let userName, greeting = 'Hello';
 
@@ -71,4 +75,65 @@ userName = prompt('Enter your name');
 alert(`${greeting}, ${userName}`);
 
 
-//
+//Condition-Based tasks
+//First task
+let usrNam;
+
+usrNam = prompt('Enter namber');
+
+if(usrNam === 10)
+{
+    console.log('Верно');
+}
+
+else
+{
+    console.log('Неверно');
+}
+
+//Second task
+function check_test_for_true(check)
+{
+    if(check === true){console.log('Верно');}
+    else{console.log('Неверно');}
+}
+
+function check_test_for_not_tru(check)
+{
+    if(check != true){console.log('Верно');}
+    else{console.log('Неверно');}
+}
+
+
+let test = true;
+console.log('Results for the first option')
+check_test_for_true(test);
+test = false;
+check_test_for_true(test);
+
+test = true;
+console.log('Results for the second option');
+check_test_for_not_tru(test);
+test = false;
+check_test_for_not_tru(test);
+
+//Third task
+const small_discount = 3, middle_discount = 5;
+let amount, toPay;
+
+amount = prompt('Enter purchase amount');
+
+if(amount >= 500 && amount < 800)
+{
+    toPay = ((amount / 100) * small_discount) - amount;
+    alert(`Purchase amount:${toPay}`)
+}
+
+else if(amount >= 800)
+{
+    toPay = ((amount / 100) * middle_discount) - amount;
+    alert(`Purchase amount:${toPay}`)
+}
+
+else{alert(`Purchase amount:${toPay}`);}
+
