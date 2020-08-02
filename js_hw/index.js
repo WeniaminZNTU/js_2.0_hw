aeeef54ac57e27e9eb97705b90e7c98a8d713fe6
Veniamin <Weniaminwelichko@ukr.net>
2020-08-03 01:05:13 +0300

    Complete variable tasks part

--- js_hw/index.js
+++ js_hw/index.js
@@ -1,1 +1,36 @@
 'use strict'
+
+//Variable tasks
+//First task
+let a = 2, b = 5;
+console.log(a * b);
+
+//Second task
+let c = 2, d = 5;
+console.log(c / d);
+
+//Third task
+let e = 2, f = 5;
+console.log(e + f);
+
+//Fourth task
+let nam = 11;
+let bool_t = true;
+let language = 'java script';
+let van_hundred = '100';
+
+console.log(nam, `: ${nam}`);
+console.log(bool_t, `: ${bool_t}`);
+console.log(language, `: ${language}`);
+console.log(van_hundred, `: ${van_hundred}`);
+
+//Sixth task
+let num = 1;
+
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
+
